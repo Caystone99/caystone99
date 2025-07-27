e65f0d00fdef66ea48f6f8606ef408fc2d06e647
Caleb-Livingstone <emmanuelcaleb546@gmail.com>
2025-07-27 10:18:02 +0100

    feat:added a header

--- ui/header.jsx
+++ ui/header.jsx
@@ -1,5 +1,22 @@
+import { GithubIcon, LinkedinIcon, MailIcon, TwitterIcon } from "lucide-react";
+
 export default function Header(){
-    return (
-        <nav></nav>
-    );
+  return (
+    <nav className="relative py-4">
+      <div className="flex w-full sticky top-10 flex-row gap-4 mx-5 md:gap-8 md:mx-20 lg:gap-12">
+        <a href='https://github.com/caystone99' target="_blank" rel="noopener noreferrer" className="text-foreground/70 hover:text-foreground">
+          <GithubIcon/>
+        </a>
+        <a href='https://x.com/caystone_99' target="_blank" rel="noopener noreferrer" className="text-foreground/70 hover:text-foreground">
+          <TwitterIcon/>
+        </a>
+        <a href='https://www.linkedin.com/in/caystone/' target="_blank" rel="noopener noreferrer" className="text-foreground/70 hover:text-foreground">
+          <LinkedinIcon/>
+        </a>   
+        <a href='mailto:emmanuelcaleblivingstone@gmail.com' target="_blank" rel="noopener noreferrer" className="text-foreground/70 hover:text-foreground">
+          <MailIcon/>
+        </a>       
+      </div>
+    </nav>
+  );
 }
